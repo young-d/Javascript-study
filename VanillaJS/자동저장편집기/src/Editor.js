1a4f8fe10c45fe6a57faa04a1e3fd48b3b509ebb
young-d <dodoga096@gmail.com>
2021-08-28 04:23:34 +0900

    contenteditable 로 rich한 에디터 만들어보기(글 추가, 수정은 안됨..)

--- VanillaJS/자동저장편집기/src/Editor.js
+++ VanillaJS/자동저장편집기/src/Editor.js
@@ -8,7 +8,11 @@ export default function Editor({
 }) {
     const $editor = document.createElement('div');
 
-    let isInitialize = false;
+    //밖에서 그려주기
+    $editor.innerHTML = `
+                <input type="text" name="title" style="width: 600px;"/>
+                <div name="content" contenteditable="true" style="width: 600px; height: 400px; border: 1px solid black; padding: 8px;"></div>
+            `;
 
     this.state = initialState;
 
@@ -16,41 +20,50 @@ export default function Editor({
 
     this.setState = nextState => {
         this.state = nextState;
-        $editor.querySelector(`[name=title]`).value = this.state.title;
-        $editor.querySelector(`[name=content]`).value = this.state.content;
+        
+        //서버에서 내려오는 개행값 \n
+        //textarea 에서는 \n으로 개행을 처리해줌 
         this.render();
     }
-
-    this.render = () => {
-        if (!isInitialize) {
-            $editor.innerHTML = `
-                <input type="text" name="title" style="width: 600px;" value="${this.state.title}"/>
-                <textarea name="content" style="width: 600px; height: 400px;">${this.state.content}</textarea>
-            `;
     
-            isInitialize = true;
-        }
+    this.render = () => {
+        const richContent = this.state.content.split('\n').map(line => {
+            if (line.indexOf('# ') === 0) {
+                return `<h1>${line.substr(2)}</h1>`;
+            } else if (line.indexOf('## ') === 0) {
+                return `<h2>${line.substr(3)}</h2>`;
+            } else if (line.indexOf('### ') === 0) {
+                return `<h3>${line.substr(4)}</h3>`;
+            }
+            return line;
+        }).join('<br>');
+
+        $editor.querySelector(`[name=title]`).value = this.state.title;
+        $editor.querySelector(`[name=content]`).innerHTML = richContent;
     };
 
     this.render();
 
-    //이벤트 등록
-    $editor.addEventListener('keyup', e => {
-        const { target } = e;
+    //title 이벤트 등록
+    $editor.querySelector('[name=title]').addEventListener('keyup', e => {
+        const nextState = {
+            ...this.state,
+            title: e.target.value
+        }
+        // console.log(nextState);
+        this.setState(nextState);
+        //편집중 이벤트
+        onEditing(this.state);
+    })
 
-        const name = target.getAttribute('name');
-
-        if (this.state[name] !== undefined) {
-            const nextState = {
-                ...this.state,
-                [name]: target.value
-            }
-            // console.log(nextState);
-            this.setState(nextState);
-            //편집중 이벤트
-            onEditing(this.state);
+    //content 이벤트 등록
+    $editor.querySelector('[name=content]').addEventListener('input', e => {
+        const nextState = {
+            ...this.state,
+            content: e.target.innerHTML
         }
 
+        this.setState(nextState);
     })
 
 }
