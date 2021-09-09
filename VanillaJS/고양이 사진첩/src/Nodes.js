3cdd536e7bbcb336044f5322cb8bfeaca0aeb177
young-d <dodoga096@gmail.com>
2021-09-09 17:51:11 +0900

    feat: App 컴포넌트에서 Nodes 컴포넌트를 생성한다.

---
+++ VanillaJS/고양이 사진첩/src/Nodes.js
@@ -0,0 +1,35 @@
+export default function Nodes({ $target, initialState, onPrevClick, onCLick }) {
+    const $nodes = document.createElement('div');
+    $target.appendChild($nodes);
+    $nodes.classList.add('nodes');
+
+    this.state = initialState;
+
+    this.setState = nextState => {
+        this.state = nextState;
+        this.render();
+    }
+
+    this.render = () => {
+        const { isRoot, nodes } = this.state;
+
+        $nodes.innerHTML = `
+            ${isRoot ? '' : `
+                <div class="Node">
+                    <image src="https://cdn.roto.codes/images/prev.png">
+                </div>
+            `}
+            ${nodes.map(node => `
+                <div class="Node">
+                    <img src="${node.type === 'DIRECTORY'
+                        ? "https://cdn.roto.codes/images/directory.png"
+                        : "https://cdn.roto.codes/images/file.png"
+                    }">
+                    ${node.name}
+                </div>
+            `).join('')}
+        `;
+    }
+
+    this.render();
+}
