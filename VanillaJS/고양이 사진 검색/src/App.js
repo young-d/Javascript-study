985a40c1a2d0300ca7465a14cb818995c03f5be1
young-d <dodoga096@gmail.com>
2021-09-10 16:13:15 +0900

    feat: 추천 키워드 api 요청하여 렌더링

--- VanillaJS/고양이 사진 검색/src/App.js
+++ VanillaJS/고양이 사진 검색/src/App.js
@@ -1,12 +1,33 @@
 import Header from "./Header.js";
+import { request } from "./api.js";
+import SuggestKeywords from "./SuggestKeywords.js";
 
 export default function App({ $target }) {
+    this.state = {
+        keywords: []
+    }
+
+    this.setState = nextState => {
+        this.state = nextState;
+        suggestKeywords.setState(this.state.keywords);
+    }
+
     const header = new Header({
         $target,
-        onKeywordInput: (keyword) => {
+        onKeywordInput: async (keyword) => {
             if (keyword.trim().length > 1) {
-                console.log(keyword);
+                const keywords = await request(`/keywords?q=${keyword}`);
+
+                this.setState({
+                    ...this.state,
+                    keywords
+                })
             }
         }
     });
+
+    const suggestKeywords =  new SuggestKeywords({
+        $target,
+        initialState: this.state.keywords
+    })
 }
---
+++ VanillaJS/고양이 사진 검색/src/SuggestKeywords.js
@@ -0,0 +1,26 @@
+export default function SuggestKeywords({ $target, initialState }) {
+    const $suggest = document.createElement('div');
+    $suggest.className = 'Keywords';
+    $target.appendChild($suggest);
+
+    this.state = initialState;
+    
+    this.setState = nextState => {
+        this.state = nextState;
+        this.render();
+    }
+
+    this.render = () => {
+        $suggest.innerHTML = `
+            <ul>
+                ${this.state.map(keyword => `
+                    <li>${keyword}</li>
+                `).join('')}
+            </ul>
+        `;
+
+        $suggest.style.display = this.state.length > 0 ? 'block' : 'none';
+    }
+
+    this.render();
+}
