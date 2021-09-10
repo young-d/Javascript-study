37da7c8b475f03c3c1c8f27d57e6dab363f0c609
young-d <dodoga096@gmail.com>
2021-09-10 15:56:55 +0900

    feat: api request 메소드 추가

---
+++ VanillaJS/고양이 사진 검색/src/api.js
@@ -0,0 +1,15 @@
+const API_END_POINT = "https://cat-search.roto.codes/api";
+
+export const request = async (url) => {
+    try {
+        const res = await fetch(`${API_END_POINT}${url}`);
+        
+        if (!res.ok) {
+            throw new Error('API 호출 실패');
+        }
+        
+        return res.json();
+    } catch (e) {
+        console.error(e.message);
+    }
+}
