706d512f5e836f2c5aef253ebdc3b5d218dbb2e1
young-d <dodoga096@gmail.com>
2021-09-13 23:45:48 +0900

    fix: api 옵션 및 헤더 추가

--- VanillaJS/TodoList_DragDrop/src/api.js
+++ VanillaJS/TodoList_DragDrop/src/api.js
@@ -1,14 +1,19 @@
-const API_END_POINT = 'https://todo-api.roto.codes/young-d';
+const API_END_POINT = 'https://todo-api.roto.codes/roto';
 
-export const request = async(url) => {
+export const request = async (url, options = {}) => {
     try {
-        const res = await fetch(`${API_END_POINT}${url}`);
+        const res = await fetch(`${API_END_POINT}${url}`, {
+            ...options,
+            headers: {
+                'Context-Type': 'application/json'
+            }
+        });
 
         if (!res.ok) {
             throw new Error('호출 실패');
         }
         return await res.json();
     } catch (e) {
-        console.error(e.message);
+        alert(e.message);
     }
 }
